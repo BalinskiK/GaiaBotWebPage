8dda27847177279e5a0c1ba58dfc9c4be457eb09
Alex Dingwall <alex7685g@icloud.com>
2024-02-02 19:24:28 +0000

    test commit for permissions

---
+++ firstfile.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>My first JavaScript</title>
+    </head>
+    <body>
+        <script>
+            document.write("Hello World!");
+        </script>
+    </body>
+</html>
