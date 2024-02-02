bdd02460b90cb377d96c4383026e48d66604b1e6
BalinskiK <balinskkami@gmail.com>
2024-02-02 20:47:28 +0000

    Flask setup

---
+++ GaiaBotFlask/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'asdifybasvkjeausdvbjsfv'
+    
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+    
+    return app
