8ff9d865310955f91659bb1097b600a5c9a05800
BalinskiK <balinskkami@gmail.com>
2024-02-04 16:16:17 +0000

    Sockets added to main - experimental

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from GaiaBotFlask import create_app
+from flask_socketio import SocketIO
 
 app = create_app()
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+@socketio.on('request_to_pi')
+def send_request_to_pi(data):
+    print('Sending request to Raspberry Pi:', data)
+    # Add logic to handle the request and control the robot
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
