9af406d7a89d18a0fff70e261bf9f135b10269e8
BalinskiK <balinskkami@gmail.com>
2024-02-06 14:51:29 +0000

    main - socket io removed

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
 from GaiaBotFlask import create_app
-from flask_socketio import SocketIO
 
 app = create_app()
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('request_to_pi')
-def send_request_to_pi(data):
-    print('Sending request to Raspberry Pi:', data)
-    # Add logic to handle the request and control the robot
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(app, debug=True)
