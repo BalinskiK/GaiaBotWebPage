27ef4da5b924f489aba5c9980cd64bb7e8681308
Alex Dingwall <alex7685g@icloud.com>
2024-02-07 12:36:40 +0000

    removed index/

--- GaiaBotFlask/views.py
+++ GaiaBotFlask/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/index')
+@views.route('/')
 def index():
     return render_template("index.html")
 
