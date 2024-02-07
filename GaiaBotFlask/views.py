22488712ba80d94cbbdb1ca748e0be97ed7cb86f
Alex Dingwall <alex7685g@icloud.com>
2024-02-07 12:13:03 +0000

    changed stylesheet on about

--- GaiaBotFlask/views.py
+++ GaiaBotFlask/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/index')
 def index():
     return render_template("index.html")
 
